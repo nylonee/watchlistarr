6ae0597eb4b3e74af6bdad97a8aa76baed462a4c
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2023-11-13 20:31:18 +0000

    memoize the config making

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,19 +1,26 @@
 
 import cats.effect._
-import configuration.{ConfigurationUtils, SystemPropertyReader}
+import configuration.{Configuration, ConfigurationUtils, SystemPropertyReader}
 import utils.HttpClient
 
 object Server extends IOApp {
   def run(args: List[String]): IO[ExitCode] = {
     val configReader = SystemPropertyReader
     val httpClient = new HttpClient()
-    val config = ConfigurationUtils.create(configReader, httpClient)
+    val configIO: IO[Configuration] = ConfigurationUtils.create(configReader, httpClient)
+    val memoizedConfigIO: IO[IO[Configuration]] = configIO.memoize
 
-    def periodicTask: IO[Unit] =
-      config.flatMap(c => WatchlistSync.run(c, httpClient)) >>
-        config.flatMap(c => IO.sleep(c.refreshInterval)) >>
-        periodicTask
+    def periodicTask(configIO: IO[Configuration]): IO[Unit] =
+      for {
+        config <- configIO
+        _ <- WatchlistSync.run(config, httpClient)
+        _ <- IO.sleep(config.refreshInterval)
+        _ <- periodicTask(configIO)
+      } yield ()
 
-    periodicTask.foreverM.as(ExitCode.Success)
+    for {
+      getConfig <- memoizedConfigIO
+      result <- periodicTask(getConfig).foreverM.as(ExitCode.Success)
+    } yield result
   }
 }
