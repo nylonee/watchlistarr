ec6a71ede2007794b0749701daf1e168e6872d6b
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2023-11-14 00:58:55 +0000

    Use a single thread pool for all client calls (#23)

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,19 +1,35 @@
 
 import cats.effect._
-import configuration.{Configuration, SystemPropertyReader}
+import configuration.{Configuration, ConfigurationUtils, SystemPropertyReader}
+import org.slf4j.LoggerFactory
 import utils.HttpClient
 
+import java.nio.channels.ClosedChannelException
+
 object Server extends IOApp {
+
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  override protected def reportFailure(err: Throwable): IO[Unit] = err match {
+    case _: ClosedChannelException => IO.pure(logger.debug("Suppressing ClosedChannelException error", err))
+    case _ => IO.pure(logger.error("Failure caught and handled by IOApp", err))
+  }
+
   def run(args: List[String]): IO[ExitCode] = {
     val configReader = SystemPropertyReader
-    val httpClient = new HttpClient()
-    val config = new Configuration(configReader, httpClient)(runtime)
+    val httpClient = new HttpClient
 
-    def periodicTask: IO[Unit] =
-      WatchlistSync.run(config) >>
-        IO.sleep(config.refreshInterval) >>
-        periodicTask
-
-    periodicTask.foreverM.as(ExitCode.Success)
+    for {
+      memoizedConfigIo <- ConfigurationUtils.create(configReader, httpClient).memoize
+      result <- periodicTask(memoizedConfigIo, httpClient).foreverM.as(ExitCode.Success)
+    } yield result
   }
+
+  private def periodicTask(configIO: IO[Configuration], httpClient: HttpClient): IO[Unit] =
+    for {
+      config <- configIO
+      _ <- WatchlistSync.run(config, httpClient)
+      _ <- IO.sleep(config.refreshInterval)
+      _ <- periodicTask(configIO, httpClient)
+    } yield ()
 }
--- src/main/scala/utils/HttpClient.scala
+++ src/main/scala/utils/HttpClient.scala
@@ -3,22 +3,19 @@ package utils
 import cats.effect.IO
 import io.circe.Json
 import org.http4s.{Header, Method, Request, Uri}
-import org.http4s.ember.client.EmberClientBuilder
 import org.typelevel.ci.CIString
 import org.http4s.circe._
-
-import scala.concurrent.duration.DurationInt
+import org.http4s.ember.client.EmberClientBuilder
 
 class HttpClient {
-  private val clientResource = EmberClientBuilder
-    .default[IO]
-    .build
+
+  val client = EmberClientBuilder.default[IO].build
 
   def httpRequest(method: Method, url: Uri, apiKey: Option[String] = None, payload: Option[Json] = None): IO[Either[Throwable, Json]] = {
     val baseRequest = Request[IO](method = method, uri = url).withHeaders(Header.Raw(CIString("Accept"), "application/json"))
     val requestWithApiKey = apiKey.fold(baseRequest)(key => baseRequest.withHeaders(Header.Raw(CIString("X-Api-Key"), key)))
     val requestWithPayload = payload.fold(requestWithApiKey)(p => requestWithApiKey.withEntity(p))
 
-    clientResource.use(_.expect[Json](requestWithPayload).attempt)
+    client.use(_.expect[Json](requestWithPayload).attempt)
   }
 }
