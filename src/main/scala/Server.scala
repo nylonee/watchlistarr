783b79d719b30193d06ce15b11b192e41d78d422
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2023-11-14 00:37:08 +0000

    try a SO answer

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -3,19 +3,22 @@ import cats.effect._
 import cats.effect.std.Semaphore
 import configuration.{Configuration, ConfigurationUtils, SystemPropertyReader}
 import org.http4s.ember.client.EmberClientBuilder
-import org.typelevel.log4cats.slf4j.Slf4jLogger
+import org.slf4j.LoggerFactory
 import utils.HttpClient
 
 object Server extends IOApp {
 
-  private val logger = Slf4jLogger.getLogger[IO]
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  override protected def reportFailure(err: Throwable): IO[Unit] = IO.pure(logger.error("IOApp reported a failure", err))
+
   def run(args: List[String]): IO[ExitCode] = {
     val maxConcurrentOutgoingRequests = 2
 
     for {
       semaphore <- Semaphore[IO](maxConcurrentOutgoingRequests)
       configReader = SystemPropertyReader
-      clientResource = EmberClientBuilder.default[IO].withLogger(logger).build
+      clientResource = EmberClientBuilder.default[IO].build
       httpClient = new HttpClient(clientResource, semaphore)
       memoizedConfigIo <- ConfigurationUtils.create(configReader, httpClient).memoize
       result <- periodicTask(memoizedConfigIo, httpClient).foreverM.as(ExitCode.Success)
