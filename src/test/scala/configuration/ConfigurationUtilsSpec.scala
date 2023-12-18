86246bd94a19bfec10fbda4bd828e0a82ce480c5
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2023-12-18 22:02:33 +0000

    fix tests

--- src/test/scala/configuration/ConfigurationUtilsSpec.scala
+++ src/test/scala/configuration/ConfigurationUtilsSpec.scala
@@ -202,13 +202,13 @@ class ConfigurationUtilsSpec extends AnyFlatSpec with Matchers with MockFactory 
       Method.POST,
       Uri.unsafeFromString("https://discover.provider.plex.tv/rss?X-Plex-Token=test-token&X-Plex-Client-Identifier=watchlistarr"),
       None,
-      Some(parse("""{"feedtype": "watchlist"}""").getOrElse(Json.Null))
+      Some(parse("""{"feedType": "watchlist"}""").getOrElse(Json.Null))
     ).returning(IO.pure(parse(Source.fromResource("rss-feed-generated.json").getLines().mkString("\n")))).anyNumberOfTimes()
     (mockHttpClient.httpRequest _).expects(
       Method.POST,
       Uri.unsafeFromString("https://discover.provider.plex.tv/rss?X-Plex-Token=test-token&X-Plex-Client-Identifier=watchlistarr"),
       None,
-      Some(parse("""{"feedtype": "friendsWatchlist"}""").getOrElse(Json.Null))
+      Some(parse("""{"feedType": "friendsWatchlist"}""").getOrElse(Json.Null))
     ).returning(IO.pure(parse(Source.fromResource("rss-feed-generated.json").getLines().mkString("\n")))).anyNumberOfTimes()
     mockHttpClient
   }
