9b4031d86d219f28ad337245bdc68180013abd41
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2024-01-05 22:41:12 +0000

    Allow deleting series that have ended/continuing

--- src/main/scala/model/Item.scala
+++ src/main/scala/model/Item.scala
@@ -1,6 +1,6 @@
 package model
 
-case class Item(title: String, guids: List[String], category: String) {
+case class Item(title: String, guids: List[String], category: String, ended: Option[Boolean] = None) {
   def getTvdbId: Option[Long] =
     guids.find(_.startsWith("tvdb://")).flatMap(_.stripPrefix("tvdb://").toLongOption)
 
@@ -10,8 +10,11 @@ case class Item(title: String, guids: List[String], category: String) {
   def getRadarrId: Option[Long] =
     guids.find(_.startsWith("radarr://")).flatMap(_.stripPrefix("radarr://").toLongOption)
 
+  def getSonarrId: Option[Long] =
+    guids.find(_.startsWith("sonarr://")).flatMap(_.stripPrefix("sonarr://").toLongOption)
+
   def matches(that: Any): Boolean = that match {
-    case Item(_, theirGuids, c) if c == this.category =>
+    case Item(_, theirGuids, c, _) if c == this.category =>
       theirGuids.foldLeft(false) {
         case (acc, guid) => acc || guids.contains(guid)
       }
--- src/main/scala/sonarr/SonarrConversions.scala
+++ src/main/scala/sonarr/SonarrConversions.scala
@@ -5,7 +5,8 @@ import model.Item
 private[sonarr] trait SonarrConversions {
   def toItem(series: SonarrSeries): Item = Item(
     series.title,
-    List(series.imdbId, series.tvdbId.map("tvdb://" + _)).collect { case Some(x) => x },
-    "show"
+    List(series.imdbId, series.tvdbId.map("tvdb://" + _), Some(s"sonarr://${series.id}")).collect { case Some(x) => x },
+    "show",
+    series.ended
   )
 }
