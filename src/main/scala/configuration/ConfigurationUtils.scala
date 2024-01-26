40335a12dde8b0f5a3a379bcfc0aebe81481956f
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2024-01-26 14:00:07 +0000

    chain

--- src/main/scala/configuration/ConfigurationUtils.scala
+++ src/main/scala/configuration/ConfigurationUtils.scala
@@ -63,8 +63,10 @@ object ConfigurationUtils {
       )
     )
 
-    config.map(c => logger.info(s"Configuration read by Watchlistarr: $c"))
-    config
+    config.map { c =>
+      logger.info(s"Configuration read by Watchlistarr: $c")
+      c
+    }
   }
 
   private def getSonarrConfig(configReader: ConfigurationReader, client: HttpClient): IO[(Uri, String, Int, String, Int)] = {
