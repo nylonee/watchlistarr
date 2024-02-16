89c5ec537f288e6903d2be8c60561b373cf37769
Nihal Mirpuri <nihal.mirpuri@gmail.com>
2024-02-16 16:15:30 +0000

    bugfix for watchlists larger than 300 items

--- src/main/scala/plex/PlexUtils.scala
+++ src/main/scala/plex/PlexUtils.scala
@@ -49,17 +49,22 @@ trait PlexUtils {
     }
   }.toList.sequence.map(_ => ())
 
-  protected def getSelfWatchlist(config: PlexConfiguration, client: HttpClient): EitherT[IO, Throwable, Set[Item]] = config.plexTokens.map { token =>
+  protected def getSelfWatchlist(config: PlexConfiguration, client: HttpClient, containerStart: Int = 0): EitherT[IO, Throwable, Set[Item]] = config.plexTokens.map { token =>
+    val containerSize = 300
     val url = Uri
       .unsafeFromString("https://metadata.provider.plex.tv/library/sections/watchlist/all")
       .withQueryParam("X-Plex-Token", token)
-      .withQueryParam("X-Plex-Container-Start", 0) // todo: pagination
-      .withQueryParam("X-Plex-Container-Size", 300)
+      .withQueryParam("X-Plex-Container-Start", containerStart)
+      .withQueryParam("X-Plex-Container-Size", containerSize)
 
     for {
       response <- EitherT(client.httpRequest(Method.GET, url))
       result <- EitherT(response.as[TokenWatchlist].map(toItems(config, client)).sequence).leftMap(err => new Throwable(err))
-    } yield result
+      nextPage <- if (result.size == containerSize)
+        getSelfWatchlist(config, client, containerStart + containerSize)
+      else
+        EitherT.pure[IO, Throwable](Set.empty[Item])
+    } yield result ++ nextPage
   }.toList.sequence.map(_.toSet.flatten)
 
   protected def getOthersWatchlist(config: PlexConfiguration, client: HttpClient): EitherT[IO, Throwable, Set[Item]] =
